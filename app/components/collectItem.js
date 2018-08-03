ca89c49e6cd68f5d7fe4174872cb2a63b89f69a2
chencm <chaoming007@163.com>
2018-08-03 17:47:24 +0800

    收藏修改

--- app/components/collectItem.js
+++ app/components/collectItem.js
@@ -21,8 +21,18 @@ export default class collectItem extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <View style={styles.box}>
-                    
+                <View style={styles.itemBox}>
+                    <View style={styles.imgWarp}>
+                        <Image source={require("../assets/img/1.png")} style={styles.imgSty} />
+                    </View>
+                    <View style={styles.txtBox}>
+                        <Text style={styles.tit1} numberOfLines={2}>
+                            卡罗拉是一个豪车，我非常喜欢这个车，这个车太好了
+                        </Text>
+                        <Text style={styles.tit2} numberOfLines={1}>
+                                卡罗拉是一个豪车，我非常喜欢这个车，这个车太好了
+                        </Text>
+                    </View>
                 </View>
             </View>
         )
@@ -31,18 +41,51 @@ export default class collectItem extends Component {
 
   const styles = StyleSheet.create({
     container: {
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
         alignItems: 'center',
         flexDirection: 'column',
-        width:width,
-        height:height,
-        backgroundColor:"#cccccc"
-        
+        width:width  
     },
-    box:{
-        width:px2dp(100),
-        height:px2dp(100),
-        backgroundColor: '#cc0000'
+    itemBox:{
+        height:px2dp(110),
+        shadowOffset:{ width:0, height:0 }, 
+        shadowColor:'#000000', 
+        shadowOpacity:0.05, 
+        shadowRadius:10,
+        marginBottom: px2dp(16),
+        width:width-32,
+        borderRadius: 5,
+        flexDirection:'row',
+        backgroundColor: "#ffffff",
+    },
+    imgWarp:{
+        width:px2dp(110),
+        height:px2dp(110),
+        marginRight: 16,
+        overflow: 'hidden',
+        borderTopLeftRadius: 5,
+        borderBottomLeftRadius: 5
+    },
+    imgSty:{
+        width:px2dp(110),
+        height:px2dp(110)
+    },
+    txtBox:{
+        flex:1,
+        flexDirection:"column",
+        marginTop: px2dp(10)
+    },
+    tit1:{
+        lineHeight:px2dp(25),
+        fontSize: px2dp(18),
+        color:"#000000",
+        marginBottom:10,
+        fontWeight:"bold"
+    },
+    tit2:{
+        lineHeight:px2dp(20),
+        fontSize: px2dp(13),
+        color:"#000000"
     }
   })
   
